a367045ccfcb0db1cab9f07371b857a3e83f6b8a
공대여자 <mins01.lycos.co.kr@gmail.com>
2019-09-24 15:04:21 +0900

    가위바위보 방식 변경.

--- popup.js
+++ popup.js
@@ -4,25 +4,35 @@
 
 'use strict';
 
+var tmp_tm = null;
+function initCode(){
+  var str =[];
+  str.push('window.tmp_tm = null');
+  str.push('window.run_rps = '+run_rps.toString()+';');
+  str.push('window.run_rpss = '+run_rpss.toString()+';');
+  str.push('window.run_rpss();');
+  return str.join("\n");
+}
+function run_rpss(){
+  console.log('run_rpss')
+  tmp_tm = setInterval(function(){
+    run_rps();
+  },1200);
+}
 
-/**
- * PHP스쿨 가위바위보
- */
-let btn_phpschool_rps = document.getElementById('btn_phpschool_rps');
-let btn_phpschool_rps_msgs = document.getElementById('btn_phpschool_rps_msgs');
-btn_phpschool_rps.onclick = function(element) {
-  var tmp_tm = setInterval(function(){
-	var u_val = 'R';
-	switch(Math.floor(Math.random()*3)){
-		case 0:u_val = 'R'; break;
-		case 1:u_val = 'S'; break;
-		case 2:u_val = 'P'; break;
-	}
+function run_rps(){
+  console.log('run_rps')
+  var u_val = 'R';
+  switch(Math.floor(Math.random()*3)){
+    case 0:u_val = 'R'; break;
+    case 1:u_val = 'S'; break;
+    case 2:u_val = 'P'; break;
+  }
   
-  btn_phpschool_rps_msgs.value = '';
+  // btn_phpschool_rps_msgs.value = '';
   $.ajax(
     {
-      url:	"https://phpschool.com/community/rock_paper_scissors_ajax.php",
+      url:	"/community/rock_paper_scissors_ajax.php",
       type:	"post",
       data:	{
         user: u_val,
@@ -30,21 +40,41 @@ btn_phpschool_rps.onclick = function(element) {
       },
       dataType:"json",
       error:	function(resData){
-        alert("오류가 발생했습니다. 관리자에게 문의하세요.");
+        alert("오류가 발생했습니다.");
+        clearInterval(tmp_tm);
+        console.log("END");
       },
       success:function(resData) {
         console.log(resData);
-        // resMsgs.push(JSON.stringify(resData))
-        btn_phpschool_rps_msgs.value = JSON.stringify(resData)+'\n'+btn_phpschool_rps_msgs.value;
         if(resData.mode && resData.com_sel){
         }else{
           clearInterval(tmp_tm);
+          alert('자동 가위바위보가 완료되었습니다.')
           console.log("END");
-          btn_phpschool_rps_msgs.value = 'END'+'\n'+btn_phpschool_rps_msgs.value;
+          window.open("/community/rock_paper_scissors.php",'_self');
         }
       }
     });
-  },1200);
+}
+
+/**
+ * PHP스쿨 가위바위보
+ */
+let btn_phpschool_rps = document.getElementById('btn_phpschool_rps');
+let btn_phpschool_rps_msgs = document.getElementById('btn_phpschool_rps_msgs');
+
+btn_phpschool_rps.onclick = function(element) {
+  var code = initCode();
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    // console.log('run script ' + txt_script.value);
+    chrome.tabs.executeScript(null, { file: "jquery-3.3.1.min.js" }, function() {
+      chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: code}
+        );
+    });
+  });
+
 }
 
 
