328cd92308044748de892367b9b5cb1b2b3a617a
공대여자 <mins01.lycos.co.kr@gmail.com>
2019-09-26 00:33:20 +0900

    modify

--- src/background.js
+++ src/background.js
@@ -1,19 +1,20 @@
 chrome.runtime.onInstalled.addListener(function() {
-	chrome.storage.sync.set({color: '#3aa757'}, function() {
-		console.log("The color is green.");
-	});
-	
+	// alert('x');
+	// chrome.storage.sync.set({color: '#3aa757'}, function() {
+	// 	console.log("The color is green.");
+	// });
+
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
+		chrome.declarativeContent.onPageChanged.addRules([{
+			conditions: [new chrome.declarativeContent.PageStateMatcher({
 				//조건이 없으면 전부
-				// pageUrl: {hostEquals: 'developer.chrome.com'}, 
-        pageUrl: {hostEquals: 'phpschool.com'}
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-	
-	
+				// pageUrl: {hostEquals: 'developer.chrome.com'},
+				pageUrl: {hostEquals: 'phpschool.com',hostEquals: 'www.phpschool.com'}
+			})
+		],
+		actions: [new chrome.declarativeContent.ShowPageAction()]
+	}]);
+});
+
+
 });
